4fe8eca256115c07d250329cf03fe87d3b1c9b9e
Tommy Hearns <hearnstj@gmail.com>
2021-04-22 23:50:07 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SunriseSunsetServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Server to get Sunrise and Sunset data for Docker project
+        }
+    }
+}
